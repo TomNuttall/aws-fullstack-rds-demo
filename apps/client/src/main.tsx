1bdc59639000aaad477aaa4a3ded208b696197fa
Tom <tsnuttall@gmail.com>
2024-07-13 19:55:48 +0100

    apps/client: Add shadcn config

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -4,13 +4,13 @@ import { ClerkProvider } from '@clerk/clerk-react'
 import ApolloProviderWrapper from './providers/ApolloProviderWrapper'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
-import './index.css'
+import './globals.css'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} telemetry={false}>
       <ApolloProviderWrapper>
         <BrowserRouter>
           <App />
