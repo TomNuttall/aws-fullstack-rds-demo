693ab912c234d01a5ccc7eff6ae15511cf482558
Tom <tsnuttall@gmail.com>
2024-07-13 19:50:40 +0100

    lambdas/api: Add query pagination tests

--- lambdas/api/tests/integration/queries.test.ts
+++ lambdas/api/tests/integration/queries.test.ts
@@ -4,53 +4,53 @@ import queries from '../../src/resolvers/queries'
 import { mockedContext } from '../mocks/mocks'
 
 describe('Query Resolvers', () => {
-  describe('GetCards', () => {
-    it('returns all cards', async () => {
+  describe('GetAllCards', () => {
+    it('returns paginated cards', async () => {
       if (!queries.getCards) return
 
       // Arrange
-      const cardFixture: Card = { id: 1, value: 1 }
+      const pageNo = 2
+      const perPage = 10
+      const cardFixture: Card = mockedContext.testData.cards[perPage]
 
       //
       const parent = {}
-      const args = {}
-      const context: Context = {
-        ...mockedContext,
-        testData: { cards: [cardFixture] },
-      }
+      const args = { pageNo, perPage }
+      const context: Context = { ...mockedContext }
 
       // Act
       // @ts-ignore
       const res = await queries.getCards(parent, args, context)
 
       // Assert
-      expect(res).toHaveLength(1)
-      expect(res[0]).toMatchObject(cardFixture)
+      expect(res.paginatedTotal).toBe(mockedContext.testData.cards.length)
+      expect(res.paginatedData).toHaveLength(perPage)
+      expect(res.paginatedData[0]).toMatchObject(cardFixture)
     })
   })
 
   describe('GetMyCards', () => {
-    it('returns all my cards', async () => {
+    it('returns my paginated cards', async () => {
       if (!queries.getMyCards) return
 
       // Arrange
-      const cardFixture: Card = { id: 1, value: 1 }
+      const pageNo = 2
+      const perPage = 7
+      const cardFixture: Card = mockedContext.testData.cards[perPage]
 
       //
       const parent = {}
-      const args = {}
-      const context: Context = {
-        ...mockedContext,
-        testData: { cards: [cardFixture] },
-      }
+      const args = { pageNo, perPage }
+      const context: Context = { ...mockedContext }
 
       // Act
       // @ts-ignore
       const res = await queries.getMyCards(parent, args, context)
 
       // Assert
-      expect(res).toHaveLength(1)
-      expect(res[0]).toMatchObject(cardFixture)
+      expect(res.paginatedTotal).toBe(mockedContext.testData.cards.length)
+      expect(res.paginatedData).toHaveLength(perPage)
+      expect(res.paginatedData[0]).toMatchObject(cardFixture)
     })
   })
 })
